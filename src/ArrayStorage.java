479ef93c53200207c3a235987bd6ce1b11fc0ac3
max <maxcappie8@gmail.com>
2020-06-18 23:05:41 +0700

    hw1 basejava

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                Arrays.fill(storage, 0, i, null);
+                break;
+            }
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                // попытка сортировки (сдвиг влево)
+                for (int j = i; j < storage.length; j++) {
+                    if (storage[j + 1] != null) {
+                        storage[j] = storage[j + 1];
+                    } else {
+                        storage[j] = null;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return Arrays.copyOfRange(storage, 0, i);
+            }
+        }
+        return Arrays.copyOf(storage, storage.length);
     }
 
     int size() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                return i;
+            }
+        }
         return 0;
     }
 }
